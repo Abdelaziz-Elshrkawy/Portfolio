ac28d64271982dda9fd859facee6baa154c20008
Abdelaziz-Elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2022-12-25 20:37:18 +0200

    adding new certificate link

--- js/app.js
+++ js/app.js
@@ -96,6 +96,21 @@ const data = {
                     link: 'https://github.com/Abdelaziz-Elshrkawy/MyStore-Udacity-Angular'
                 }
             ]
+        },
+        {
+            name: 'Professional Front-End Web Development',
+            link: 'https://graduation.udacity.com/confirm/LRVLGJCE',
+            date: 'DEC 2022',
+            projects: [
+                {
+                    name: 'Landing Page (Portfolio)',
+                    link: 'https://github.com/Abdelaziz-Elshrkawy/Portfolio'
+                },
+                {
+                    name: 'Weather Journal App',
+                    link: 'https://github.com/Abdelaziz-Elshrkawy/weather-journal-app'
+                }
+            ]
         }
     ],
     provider: [
@@ -174,7 +189,7 @@ const certificatesContent = (() => {
         div.setAttribute('class', 'container')
         projectsDiv.setAttribute('class', 'project-div')
         //main text
-        p.innerHTML = `<span>${data.certificates[i].name}</span> track on ${data.certificates[i].date} provided by <a target='blank' href='${data.provider[0].website}'>
+        p.innerHTML = `<span>${data.certificates[i].name}</span> track on (${data.certificates[i].date}) provided by <a target='blank' href='${data.provider[0].website}'>
                         <img id=${data.provider[0].name} src='${data.provider[0].logo}' alt='provider'  height='30'/></a>  and sponsored by 
                         <a target='blank' href='${data.provider[1].website}'><img id=${data.provider[1].name} src='${data.provider[1].logo}' alt='provider'  height='50'/></a>`
         div.appendChild(p)
